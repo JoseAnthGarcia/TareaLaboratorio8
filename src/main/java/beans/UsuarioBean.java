0d46f604695d24e581e2ef83b9849c42749cad6a
Jose Garcia <garcia.josea@pucp.edu.pe>
2020-11-15 18:39:56 -0500

    faltan las validaciones correspondientes al registro de un usuario

---
+++ src/main/java/beans/UsuarioBean.java
@@ -0,0 +1,58 @@
+package beans;
+
+public class UsuarioBean {
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String correo;
+    private String contrasenia;
+    private int idDistrito;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public int getIdDistrito() {
+        return idDistrito;
+    }
+
+    public void setIdDistrito(int idDistrito) {
+        this.idDistrito = idDistrito;
+    }
+}
