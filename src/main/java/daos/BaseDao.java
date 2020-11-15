615a9ab892b2c01182ec5691a39069b4cc677f01
Jose Garcia <garcia.josea@pucp.edu.pe>
2020-11-15 17:14:44 -0500

    cambie a abstract BaseDao.java

--- src/main/java/daos/BaseDao.java
+++ src/main/java/daos/BaseDao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class BaseDao {
+public abstract class BaseDao {
     public Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
