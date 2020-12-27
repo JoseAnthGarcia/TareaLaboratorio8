067b588514c04ed3aa6135afae963b17975fd1d8
Jose Garcia <garcia.josea@pucp.edu.pe>
2020-12-27 18:42:39 -0500

    -

--- src/main/java/servlets/Bodega/BodegaServlet.java
+++ src/main/java/servlets/Bodega/BodegaServlet.java
@@ -237,6 +237,12 @@ public class BodegaServlet extends HttpServlet {
             throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
 
+        response.addHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.addHeader("Cache-Control", "pre-check=0, post-check=0");
+        response.setDateHeader("Expires", 0);
+
+
         HttpSession session = request.getSession();
         BodegaBean bodegaActual = (BodegaBean) session.getAttribute("bodega");
 
--- src/main/java/servlets/Cliente/LoginServlet.java
+++ src/main/java/servlets/Cliente/LoginServlet.java
@@ -167,6 +167,11 @@ public class LoginServlet extends HttpServlet {
             throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
 
+        response.addHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.addHeader("Cache-Control", "pre-check=0, post-check=0");
+        response.setDateHeader("Expires", 0);
+
         HttpSession session = request.getSession();
         RequestDispatcher view2;
         UsuarioDao usuarioDao = new UsuarioDao();
