7db3af44f6e1b39c848eec33816c88be130e5d55
Jose Garcia <garcia.josea@pucp.edu.pe>
2020-12-27 18:11:40 -0500

    -

--- src/main/java/daos/UsuarioDao.java
+++ src/main/java/daos/UsuarioDao.java
@@ -533,22 +533,27 @@ public class UsuarioDao extends BaseDao {
     }
 
     public String generarCodigoPedido(){
-        // Los caracteres de interés en un array de char.
-        char[] chars = "0123456789".toCharArray();
-        // Longitud del array de char.
-        int charsLength = chars.length;
-        // Instanciamos la clase Random
-        Random random = new Random();
-        // Un StringBuffer para componer la cadena aleatoria de forma eficiente
-        StringBuffer buffer = new StringBuffer();
-        // Bucle para elegir una cadena de 10 caracteres al azar
-        for (int i = 0; i < 9; i++) {
-            // Añadimos al buffer un caracter al azar del array
-            buffer.append(chars[random.nextInt(charsLength)]);
-        }
+        String codigo = "";
+        do{ //bucle para no repetir codigos generados
+            // Los caracteres de interés en un array de char.
+            char[] chars = "0123456789".toCharArray();
+            // Longitud del array de char.
+            int charsLength = chars.length;
+            // Instanciamos la clase Random
+            Random random = new Random();
+            // Un StringBuffer para componer la cadena aleatoria de forma eficiente
+            StringBuffer buffer = new StringBuffer();
+            // Bucle para elegir una cadena de 10 caracteres al azar
+            for (int i = 0; i < 9; i++) {
+                // Añadimos al buffer un caracter al azar del array
+                buffer.append(chars[random.nextInt(charsLength)]);
+            }
+            codigo = buffer.toString();
+        }while(obtenerPedido(codigo)!=null);
+
         // Y solo nos queda hacer algo con la cadena
         //System.out.println(buffer.toString());
-        return buffer.toString();
+        return codigo;
     }
 
     public int crearPedido(PedidoBean pedido){
